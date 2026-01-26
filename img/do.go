c1ba00b79d7bf27fe51961b789598940fdbead17
XV-521 <lingyigexiaov@icloud.com>
2026-01-26 09:31:01 +0800

    bug fixes

--- img/do.go
+++ img/do.go
@@ -39,7 +39,7 @@ func DoBatch(md Mode) error {
 			dstPath := filepath.Join(md.DstDir, entry.Name())
 			return resize(srcPath, dstPath, md.Rto)
 		}
-		err := internal.DoBatchWrapper(md.SrcDir, bm, filter, resizeHandler)
+		err := internal.DoBatchWrapper(getSrcDir(), bm, filter, resizeHandler)
 		if err != nil {
 			return err
 		}
@@ -52,7 +52,7 @@ func DoBatch(md Mode) error {
 			dstPath := filepath.Join(md.DstDir, entry.Name())
 			return changeDpi(srcPath, dstPath, md.DPI)
 		}
-		err := internal.DoBatchWrapper(md.DstDir, bm, filter, changeDpiHandler)
+		err := internal.DoBatchWrapper(getSrcDir(), bm, filter, changeDpiHandler)
 		if err != nil {
 			return err
 		}
